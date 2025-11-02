0966c31c7b9101fc5c9c2a7e0785420e1d504233
Juliette De Meo <gigialiasjuliette@gmail.com>
2025-11-02 16:52:55 +0100

    Hello world

---
+++ src/main.jsx
@@ -0,0 +1,40 @@
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+import App from "./App";
+
+// page components
+
+// router creation
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/projects",
+        element: <Projects />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+// rendering
+
+const rootElement = document.getElementById("root");
+
+if (rootElement != null) {
+  ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
+}
